cf833fd9b26a59e65e733c728ad3f47935233079
Mateusz Koczan <mateusz@wildland.io>
2020-07-03 16:27:03 +0200

    Add emoji conversion

--- tweetbrief/twitterapi/simple_tweet.py
+++ tweetbrief/twitterapi/simple_tweet.py
@@ -1,19 +1,21 @@
 import html
 from typing import List
 
+import emoji
+
 from utils.regex_patterns import http_pattern, url_pattern
 
 
 class SimpleTweet:
-    def __init__(self, id: str, uid: str, author: str, text: str,
-                retweet_count: int, favorite_count: int,
-                created_at: str) -> None:
+    def __init__(
+        self, id: str, uid: str, author: str, text: str, retweet_count: int, favorite_count: int, created_at: str
+    ) -> None:
         self.id = id
-        self.uid = uid        
+        self.uid = uid
         self.author = author
         self.text = self._cleanse(text)
         self.retweet_count = retweet_count
-        self.favorite_count = favorite_count  
+        self.favorite_count = favorite_count
         self.created_at = created_at
 
     def extract_urls(self, remove_http: bool = False) -> List[str]:
@@ -27,4 +29,6 @@ class SimpleTweet:
         self.text = url_pattern.sub(value, self.text)
 
     def _cleanse(self, text: str) -> str:
-        return " ".join(html.unescape(text).split())
+        cleansed_text = " ".join(html.unescape(text).split())
+        cleansed_text = emoji.demojize(cleansed_text)
+        return cleansed_text
